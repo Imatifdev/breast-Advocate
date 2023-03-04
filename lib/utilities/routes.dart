b64b219fe7af6e5e23fa4541c9246ebfcd8fa063
matifpervaizofficial <matifpervaizofficial@gmail.com>
2023-03-05 02:16:12 +0500

    calculator update

---
+++ lib/utilities/routes.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/foundation.dart';
+import 'package:get/get.dart';
+import 'package:quizapp2/view/screens/cancerscreens/excerise.dart';
+import 'package:quizapp2/view/screens/cancerscreens/intro.dart';
+
+appRoutes() => [
+      GetPage(
+        name: '/Intro',
+        page: () => Intro(),
+        transition: Transition.leftToRightWithFade,
+        transitionDuration: Duration(milliseconds: 500),
+      ),
+      GetPage(
+        name: '/Exercise',
+        page: () => MyCustomWidget(),
+        transition: Transition.leftToRightWithFade,
+        transitionDuration: Duration(milliseconds: 500),
+      ),
+    ];
+
+class MyMiddelware extends GetMiddleware {
+  @override
+  GetPage? onPageCalled(GetPage? page) {
+    if (kDebugMode) {
+      print(page?.name);
+    }
+    return super.onPageCalled(page);
+  }
+}
